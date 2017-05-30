224eb9d9cc7405423a9470fcff2e924d356d35ae
Brian Fajardo <bmf5@hotmail.com>
2017-05-29 20:18:15 -0400

    Query age range and years active range

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -1,9 +1,25 @@
-const Artist = require('../models/artist');
+const Artist = require('../models/artist')
 
 /**
  * Finds the lowest and highest age of artists in the Artist collection
  * @return {promise} A promise that resolves with an object
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
+
 module.exports = () => {
-};
+  const minAge = Artist.find({})
+    .sort({ age: 1 }) // Ascending age
+    .limit(1)
+    .then(artists => artists[0].age)
+
+  const maxAge = Artist.find({})
+    .sort({ age: -1 }) // Descending age
+    .limit(1)
+    .then(artists => artists[0].age)
+
+  return Promise.all([minAge, maxAge])
+    .then(results => ({
+      min: results[0],
+      max: results[1]
+    }))
+}
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -1,9 +1,25 @@
-const Artist = require('../models/artist');
+const Artist = require('../models/artist')
 
 /**
  * Finds the lowest and highest yearsActive of artists in the Artist collection
  * @return {promise} A promise that resolves with an object
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
+
 module.exports = () => {
-};
+  const minYearsActive = Artist.find({})
+    .sort({ yearsActive: 1 })
+    .limit(1)
+    .then(artists => artists[0].yearsActive)
+
+  const maxYearsActive = Artist.find({})
+    .sort({ yearsActive: -1 })
+    .limit(1)
+    .then(artists => artists[0].yearsActive)
+
+  return Promise.all([minYearsActive, maxYearsActive])
+    .then(results => ({
+      min: results[0],
+      max: results[1]
+    }))
+}
